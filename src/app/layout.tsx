327f81c43f474e1560658b2adeece5deb52e43b2
Workshop Participant <participant@example.com>
2024-12-05 00:44:59 +0000

    Added Amplify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,10 @@ import React from 'react';
 import './globals.css';
 import '@aws-amplify/ui-react/styles.css'
 import { Header, Footer } from '../components';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
 
+Amplify.configure(outputs, {ssr: true});
 
 export default function RootLayout({
   children,
